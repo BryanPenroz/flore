6386e5ce4aabbf067f08c3068ac5ab6d5dbc4366
bastiankusch <54455413+bastiankusch@users.noreply.github.com>
2019-12-13 14:41:55 -0300

    Avance de carrito de compras
    Se avanza en carrito de compras

--- myProyecto/petalos/urls.py
+++ myProyecto/petalos/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import index,gale,formulario,login,login_acceso,cerrar_sesion,eliminar_flores,agregar_carrito,registro_usuario
+from .views import index,gale,formulario,login,login_acceso,cerrar_sesion,eliminar_flores,registro_usuario
 
 urlpatterns = [
     path('', index,name='IND'),
@@ -10,6 +10,8 @@ urlpatterns = [
     path('login_acceso/',login_acceso,name='LOGINACCESO'),
     path('cerrar_sesion/',cerrar_sesion,name='CERRARSESION'),
     path('eliminar_flores/<id>/',eliminar_flores,name='ELIMINA'),
-    path('agregar_carrito/<id>/',agregar_carrito,name='AGREGAR'),
-    path('registro/',registro_usuario,name='REGISTRO')
+    path('registro/',registro_usuario,name='REGISTRO'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,name='remove-single-item-from-cart'),
 ]
--- myProyecto/petalos/views.py
+++ myProyecto/petalos/views.py
@@ -43,23 +43,6 @@ def login_iniciar(request):
         }
         return remder(request,'core/login.html',variables)          
 
-@login_required(login_url='/login/')
-def agregar_carrito(request,id):
-    #recuperar la lista del carrito desde la sesion
-    lista=request.session.get("carrito","")
-    #agregar el titulo a listado
-    lista=lista+str(id)+str(";")
-    #volver a ingresarlo a la sesion
-    request.session["carrito"]=lista
-    return render(request,"core/carrito.html",{'listaCarrito':lista})
-    
-def carros(request):
-    x=request.session["carritox"]
-    suma=0
-    for item in x:
-        suma=suma+int(item["total"])
-    return render(request,'core/carrito.html',{'x':x,'total':suma})    
-
 def eliminar_flores(request,id):
     flor=Flores.objects.get(name=id)#buscar flor
     msg=''
@@ -121,8 +104,97 @@ def formulario(request):
             return render(request,'core/formulario.html',{'listacategoria':categorias,'msg':'grabo'})
         if accion=='eliminar':
             titulo=request.POST.get("txtTitulo")#recupera el titulo
-            flor=Flores.objects.get(name=titulo)# lo busca entre las peliculas
+            flor=Flores.objects.get(name=titulo)# lo busca entre las flores
             flor.delete()#elimina
             return render(request,'core/formulario.html',{'listacategoria':categorias,'msg':'elimino'})
         return render(request,'core/formulario.html',{'listacategoria':categorias})
     return render(request,'core/formulario.html',{'listacategoria':categorias})
+
+
+
+@login_required
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False
+    )
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.info(request, "This item quantity was updated.")
+            return redirect("core:order-summary")
+        else:
+            order.items.add(order_item)
+            messages.info(request, "This item was added to your cart.")
+            return redirect("core:order-summary")
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(
+            user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
+        messages.info(request, "This item was added to your cart.")
+        return redirect("core:order-summary")
+
+
+@login_required
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            order.items.remove(order_item)
+            messages.info(request, "This item was removed from your cart.")
+            return redirect("core:order-summary")
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect("core:product", slug=slug)
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("core:product", slug=slug)
+
+
+@login_required
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+            else:
+                order.items.remove(order_item)
+            messages.info(request, "This item quantity was updated.")
+            return redirect("core:order-summary")
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect("core:product", slug=slug)
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("core:product", slug=slug)
