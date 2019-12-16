ee5279a0621fbf6257440d172c3d0df218606d54
BryanPenroz <49246523+BryanPenroz@users.noreply.github.com>
2019-12-16 14:34:13 -0300

    Revert "Revert "Avance Carro de compras""
    This reverts commit 85dbe8924eb3027b9dcf2d6f0dc443292f347618.

--- myProyecto/petalos/admin.py
+++ myProyecto/petalos/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Categoria, Flores
+from .models import Categoria, Flores, Ticket
 
 # Register your models here.
 admin.site.register(Flores)
 admin.site.register(Categoria)
+admin.site.register(Ticket)
 
